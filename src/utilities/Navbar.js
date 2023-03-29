f38d91c10663f51bc037515d311832ee47cf7665
Kiran Kumar <kirankumar021097@gmail.com>
2023-03-29 13:11:22 +0530

    first commit

---
+++ src/utilities/Navbar.js
@@ -0,0 +1,15 @@
+import {
+    Row
+} from 'react-bootstrap'
+import React from 'react'
+function Navbar() {
+  return (
+    <div>
+        <div className="bg-dark" style={{ width: '100%', backgroundColor: '#19A7CE' }}>
+            Navbar
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
